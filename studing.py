4d632027603848cc09f45c79410c6fd0664f27b5
OlegGdalevich <gdalevich.oleg@gmail.com>
2026-02-25 22:09:39 +0300

    the end

--- studing.py
+++ studing.py
@@ -1,38 +1,32 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from time import time
 
-st.title('st.cache')
+st.title('🖼️ yt-img-app')
+st.header('YouTube Thumbnail Image Extractor App')
 
-# Using cache
-a0 = time()
-st.subheader('Using st.cache_data')
+with st.expander('About this app'):
+  st.write('This app retrieves the thumbnail image from a YouTube video.')
 
-@st.cache_data
-def load_data_a():
-  df = pd.DataFrame(
-    np.random.rand(2000000, 5),
-    columns=['a', 'b', 'c', 'd', 'e']
-  )
-  return df
+# Image settings
+st.sidebar.header('Settings')
+img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
+selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
+img_quality = img_dict[selected_img_quality]
 
-st.write(load_data_a())
-a1 = time()
-st.info(a1-a0)
+yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/JwSS70SZdyM')
 
+def get_ytid(input_url):
+  if 'youtu.be' in input_url:
+    ytid = input_url.split('/')[-1]
+  if 'youtube.com' in input_url:
+    ytid = input_url.split('=')[-1]
+  return ytid
 
-# Not using cache
-b0 = time()
-st.subheader('Not using st.cache')
+# Display YouTube thumbnail image
+if yt_url != '':
+  ytid = get_ytid(yt_url) # yt or yt_url
 
-def load_data_b():
-  df = pd.DataFrame(
-    np.random.rand(2000000, 5),
-    columns=['a', 'b', 'c', 'd', 'e']
-  )
-  return df
-
-st.write(load_data_b())
-b1 = time()
-st.info(b1-b0)
+  yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
+  st.image(yt_img)
+  st.write('YouTube video thumbnail image URL: ', yt_img)
+else:
+  st.write('☝️ Enter URL to continue ...')
